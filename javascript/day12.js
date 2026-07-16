7d5f07685b94fb60a533a483d903d57393fe94e6
kangfadho <152151825+kangfadho@users.noreply.github.com>
2026-07-16 14:16:17 +0700

    belajar day12

--- javascript/day12.js
+++ javascript/day12.js
@@ -1,2 +1,11 @@
-let array = [1, 2, 3, 4]
-  array.forEach(e, )
+let saldo = 10000;
+let biayaAdmin = 5000;
+let tarikTunai = 5000;
+let sisaSaldo = saldo - biayaAdmin - tarikTunai;
+
+if (saldo >= tarikTunai){
+  console.log('Saldo Anda: '+ saldo + (' Tarik Tunai: '+ tarikTunai + 'Sisa Saldo Lu: ' + sisaSaldo ));
+} else {
+  console.log('Saldo tidak Cukup AJg');
+  console.log('Saldo lU Segini Ajg :' + saldo)
+}
